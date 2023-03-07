c618f35434b5b71ec73372506be4480dfc0fc403
Hariharan0007 <=>
2023-03-07 13:35:12 +0530

    added register

--- app/src/main/java/com/example/harvestmate/MainActivity.java
+++ app/src/main/java/com/example/harvestmate/MainActivity.java
@@ -7,23 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView register;
+    private TextView register_text;
+    private Button register_farmer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        register = findViewById(R.id.farmer_reg);
+        register_text = findViewById(R.id.farmer_reg);
 
-        register.setOnClickListener(new View.OnClickListener() {
+        register_text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 setContentView(R.layout.farmer_registration);
             }
         });
+
     }
 }
