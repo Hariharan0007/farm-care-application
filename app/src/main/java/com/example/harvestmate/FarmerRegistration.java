c266720788c760a0f429d8bfbcd6e4a8e25d4219
Hariharan0007 <=>
2023-03-07 13:35:39 +0530

    added register class

---
+++ app/src/main/java/com/example/harvestmate/FarmerRegistration.java
@@ -0,0 +1,35 @@
+package com.example.harvestmate;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class FarmerRegistration extends AppCompatActivity{
+
+    private Button register_farmer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.farmer_registration);
+
+        register_farmer = findViewById(R.id.farmer_register_btn);
+
+        register_farmer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FarmerRegistration.this,"Successfully registered!!!",Toast.LENGTH_SHORT).show();
+                setContentView(R.layout.activity_main);
+            }
+        });
+
+
+    }
+}
